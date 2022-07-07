47005364bee1ea72fd81162cc416744eb5708b58
RODOLFO LUIZ ALVARENGA <t-rodolfo.alvarenga@bradesco.com.br>
2022-07-07 17:11:11 -0300

    02.07. Trabalhando com variáveis

---
+++ ej/ej/src/main/java/aafundamentosdalinguagemjava/AulaVariaveis.java
@@ -0,0 +1,17 @@
+package aafundamentosdalinguagemjava;
+
+public class AulaVariaveis {
+
+    public static void main(String[] args) {
+        // Não recomendado pela Code Conventions da Oracle
+        // int minhaIdade = 40, suaIdade = 25;
+
+        int minhaIdade = 40;
+        int suaIdade = 25;
+
+        int totalIdades = minhaIdade + suaIdade;
+
+        System.out.println("Total das idades: " + totalIdades);
+    }
+
+}
