2b989edfe4b753f7e9df802da6cc297cfc1fc209
RODOLFO LUIZ ALVARENGA <t-rodolfo.alvarenga@bradesco.com.br>
2022-07-07 11:54:02 -0300

    02.04. Escrevendo comentários no código

---
+++ ej/ej/src/main/java/aafundamentosdalinguagemjava/OlaMergulhadorComentarios.java
@@ -0,0 +1,14 @@
+package aafundamentosdalinguagemjava;
+
+// 02.04. Escrevendo comentários no código
+
+// Você pode comentar uma linha inteira
+public class OlaMergulhadorComentarios { // ou na frente de uma declaração ou instrução
+
+    /*
+    public static void main(String[] args) {
+        System.out.println("Olá, mergulhador!");
+    }
+    */
+
+}
