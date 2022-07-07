69bcdd53335997949374f2f0c3ec1a22801ca8ff
RODOLFO LUIZ ALVARENGA <t-rodolfo.alvarenga@bradesco.com.br>
2022-07-07 09:13:30 -0300

    02.01. Criando o primeiro programa Java

---
+++ ej/ej/src/main/java/aafundamentosdalinguagemjava/OlaMergulhador.java
@@ -0,0 +1,11 @@
+package aafundamentosdalinguagemjava;
+
+// 02.01. Criando o primeiro programa Java
+
+public class OlaMergulhador {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mergulhador!");
+    }
+
+}
