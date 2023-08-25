4e7ea713d04f9a93fe44fa7b9ab44b8442311e99
Rodolfo Alvarenga <rodolfo.alvarenga@br.bosch.com>
2023-08-25 12:00:54 -0300

    2.11. Operadores de incremento e decremento

---
+++ src/a_fundamentosdalinguagemjava/OperadorDecremento.java
@@ -0,0 +1,17 @@
+package a_fundamentosdalinguagemjava;
+
+public class OperadorDecremento {
+
+    public static void main(String[] args) {
+        int limiteTentativasLogin = 10;
+
+        // limiteTentativasLogin--;
+
+        // int novoLimiteTentativasLogin = limiteTentativasLogin--;
+        int novoLimiteTentativasLogin = --limiteTentativasLogin;
+
+        System.out.println("Limite de tentativas de login: " + limiteTentativasLogin);
+        System.out.println("Novo limite de tentativas de login: " + novoLimiteTentativasLogin);
+    }
+
+}
---
+++ src/a_fundamentosdalinguagemjava/OperadorIncremento.java
@@ -0,0 +1,20 @@
+package a_fundamentosdalinguagemjava;
+
+public class OperadorIncremento {
+
+    public static void main(String[] args) {
+        int totalDownloads = 10;
+
+        // totalDownloads = totalDownloads + 1;
+        // totalDownloads += 1;
+
+        // totalDownloads++;
+
+        // int novoTotalDownloads = totalDownloads++;
+        int novoTotalDownloads = ++totalDownloads;
+
+        System.out.println("Total de downloads: " + totalDownloads);
+        System.out.println("Novo total de downloads: " + novoTotalDownloads);
+    }
+
+}
