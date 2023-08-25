1b1e7fc93e25553b129fd9d1211df1d48745e7ef
Rodolfo Alvarenga <rodolfo.alvarenga@br.bosch.com>
2023-08-25 08:35:02 -0300

    2.4. Escrevendo comentários no código

--- src/a_fundamentosdalinguagemjava/OlaMergulhador.java
+++ src/a_fundamentosdalinguagemjava/OlaMergulhador.java
@@ -1,8 +1,14 @@
 package a_fundamentosdalinguagemjava;
 
-public class OlaMergulhador {
+// Você pode comentar uma linha inteira
+public class OlaMergulhador { // ou na frente de uma declaração ou instrução
 
+    /*
+    Método principal do programa.
+    Ao executar o programa, esse método será chamado automaticamente
+    */
     public static void main(String[] args) {
+        // imprime a mensagem na saída padrão
         System.out.println("Olá, mergulhador!");
     }
 
